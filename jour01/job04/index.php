3f6d0ff475bd4ae63c5dfc659e16f0f25e6fe0ab
Pawel Barc <pawel.barc@laplateforme.io>
2024-09-03 10:11:48 +0200

    Job four of day one done

---
+++ jour01/job04/index.php
@@ -0,0 +1,21 @@
+<?php
+
+function my_fizz_buzz(int $length) : array {
+    $result = []; 
+
+    for ($i = 1; $i <= $length; $i++) {
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            $result[] = 'FizzBuzz'; 
+        } elseif ($i % 3 === 0) {
+            $result[] = 'Fizz';
+        } elseif ($i % 5 === 0) {
+            $result[] = 'Buzz';
+        } else {
+            $result[] = $i; 
+        }
+    }
+
+    return $result; 
+}
+
+var_dump(my_fizz_buzz(35)); 
